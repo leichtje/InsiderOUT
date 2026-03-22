caf3d5bc5c95529e57a1b103c30735f2cf2a46d7
LoganFree <148898734+LoganFree@users.noreply.github.com>
2026-03-22 12:56:06 -0400

    Incident Heat Map

--- insider_out.client/src/app/fragments/incidents-heat-map/incidents-heat-map.component.ts
+++ insider_out.client/src/app/fragments/incidents-heat-map/incidents-heat-map.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from "@angular/core";
+import { Component, computed, input } from "@angular/core";
 import { IncidentModel } from "../../models/incidents.model";
 
 @Component({
@@ -11,5 +11,43 @@ import { IncidentModel } from "../../models/incidents.model";
 export class IncidentsHeatMapComponent {
 
     readonly allIncidents$ = input.required<IncidentModel[]>({alias: 'allIncidents'});
+
+    incidentCountsByDay = computed(() => {  
+        const allIncidents = this. allIncidents$();
+        const counts = new Map<string, number>();
+
+        if (!allIncidents) return counts;
+
+        allIncidents.forEach(incident => {
+            const dateString = new Date(incident.date).toISOString().split('T')[0];
+            
+            const currentCount = counts.get(dateString) || 0;
+            counts.set(dateString, currentCount + 1);
+        });
+
+        return counts;
+    });
+
+    daysInCurrentMonth = computed(() => {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = now.getMonth(); 
+        
+        const totalDays = new Date(year, month + 1, 0).getDate(); 
+        
+        return Array.from({ length: totalDays }, (_, i) => {
+            const day = i + 1;
+            const dateString = `${year}-${String(month + 1).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+        
+            return { date: dateString, dayOfMonth: day };
+        });
+    });
+
+    getColorTier(count: number): string {
+        if (count === 0) return 'tier-0'; // No incidents
+        if (count <= 2) return 'tier-1';  // Low
+        if (count <= 5) return 'tier-2';  // Medium
+        return 'tier-3';                  // High
+    }
 
 }
