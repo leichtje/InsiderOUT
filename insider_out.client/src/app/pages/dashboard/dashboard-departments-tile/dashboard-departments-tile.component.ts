aea5c27ce46baa5d192821fe516166d6f486a07c
LoganFree <148898734+LoganFree@users.noreply.github.com>
2026-03-22 16:28:13 -0400

    Work for Dept Tile

--- insider_out.client/src/app/pages/dashboard/dashboard-departments-tile/dashboard-departments-tile.component.ts
+++ insider_out.client/src/app/pages/dashboard/dashboard-departments-tile/dashboard-departments-tile.component.ts
@@ -1,19 +1,26 @@
 
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { IncidentModel } from '../../../models/incidents.model';
 import { MatTabsModule } from '@angular/material/tabs';
 import { DepartmentDashboards } from '../../../stores/dashboard.store';
+import { AppRoutingModule } from "../../../app-routing.module";
+import { RouterLink } from '@angular/router';
+import { PercentRingComponent } from "../../../fragments/percent-ring/percent-ring.component";
 
 @Component({
     selector: 'io-dashboard-departments-tile',
     templateUrl: './dashboard-departments-tile.component.html',
     styleUrl: './dashboard-departments-tile.component.scss',
     standalone: true,
-    imports: [MatTabsModule]
+    imports: [MatTabsModule, RouterLink, PercentRingComponent]
 })
 export class DashboardDepartmentsTileComponent {
 
     readonly departmentDashboards$ = input.required<DepartmentDashboards[]>({alias: 'departmentDashboards'});
 
+    expandedDepartmentId = signal<number | null>(null);
 
+    toggleDepartment(id: number) {
+        this.expandedDepartmentId.update(currentId => currentId === id ? null : id);
+    }
 }
