59d1854c149d3e9d76a4b36701bb4030d34327a1
LoganFree <148898734+LoganFree@users.noreply.github.com>
2026-03-22 20:10:37 -0400

    Update To Subject Tile

--- insider_out.client/src/app/pages/dashboard/dashboard-risk-breakdown/dashboard-risk-breakdown-tile.component.ts
+++ insider_out.client/src/app/pages/dashboard/dashboard-risk-breakdown/dashboard-risk-breakdown-tile.component.ts
@@ -1,19 +1,27 @@
 
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { PercentRingComponent } from "../../../fragments/percent-ring/percent-ring.component";
 import { DepartmentDashboards } from '../../../stores/dashboard.store';
 import { SubjectModel } from '../../../models/profile.model';
+import { RouterLink } from '@angular/router';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
     selector: 'io-dashboard-risk-breakdown-tile',
     templateUrl: './dashboard-risk-breakdown-tile.component.html',
     styleUrl: './dashboard-risk-breakdown-tile.component.scss',
     standalone: true,
-    imports: [PercentRingComponent]
+    imports: [PercentRingComponent, RouterLink, MatIcon]
 })
 export class DashboardRiskBreakdownComponent {
 
     readonly overallCompanyRisk$ = input.required<number>({alias: 'overallCompanyRisk'})
     readonly topSubjects$ = input.required<SubjectModel[] | null>({alias: 'topSubjects'})
+
+    expandedSubjectId = signal<number | null>(null);
+
+    toggleSubject(id: number) {
+        this.expandedSubjectId.update(currentId => currentId === id ? null : id);
+    }
 
 }
